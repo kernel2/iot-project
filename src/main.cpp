1183c8cd2917082e3248d95ce96ef2fbef6025d3
unknown <s8f@hotmail.fr>
2021-04-22 13:05:18 +0200

    publish distance and subscribe

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,28 @@
+#include <Arduino.h>
+#include <stdlib.h>
 #include <PubSubClient.h>
+#include <EthernetClient.h>
 
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-IPAddress server(172, 17, 0, 4);
+#include <Ethernet.h>
 
-#define LED_PIN D4
+#define ECHO_PIN 11
+#define TRIG_PIN 12
 
-int trig = 12;
-int echo = 13;
-long lecture_echo;
-long cm;
-const char *ssid = "HUAWEI P20 lite"; //Your WiFI ssid
-const char *password = "a1234567";    //Your WiFi password
+const int LED_PIN = 7;
+const int BUZZER_PIN = 3;
 
-const char *mqttServer = "172.17.0.4";
-const int mqttPort = 1883;
-const char *mqttUser = "admin";
-const char *mqttPassword = "admin";
+float time_echo = 0;
+float distance = 0;
 
-WiFiClient espClient;
-PubSubClient client(espClient);
+const char *clientServer = "10.0.2.2";
+const int clientPort = 1883;
+const char *clientUser = "admin";
+const char *clientPassword = "admin";
+
+Ethernet WSA;
+EthernetClient ethClient;
+PubSubClient mqttClient;
+
 
 String payloadToString(byte *payload, unsigned int length)
 {
@@ -28,98 +31,115 @@ String payloadToString(byte *payload, unsigned int length)
   return String(buffer);
 }
 
-void callback(char *topic, byte *payload, unsigned int length)
+
+void callback(char *topicChar, byte *payload, unsigned int length)
 {
   String message = payloadToString(payload, length);
-  if (message.equals("on"))
-  {
-    digitalWrite(LED_PIN, LOW);
-  }
-  else if (message.equals("off"))
-  {
-    digitalWrite(LED_PIN, HIGH);
-  }
+  String topic = String(topicChar);
+  if(!topic.equals("captureLED")){
+    return;
+  } else if(topic.equals("captureLED")){
+        if (message.equals("on"))
+        {
+          digitalWrite(LED_PIN, HIGH);
+          digitalWrite(BUZZER_PIN, HIGH);
+        } else if (message.equals("off")) {
+          digitalWrite(LED_PIN, LOW);
+          digitalWrite(BUZZER_PIN, LOW);
+        } 
+    }
 }
 
-PubSubClient mqtt(server, 1883, callback, espClient);
+void setupMqttClient() {
+  mqttClient.setClient(ethClient);
+  mqttClient.setServer(clientServer,clientPort);
+  mqttClient.setCallback(callback);
+}
+
 
 void setup()
 {
   Serial.begin(9600);
 
   //capteur de distance
-  pinMode(trig, OUTPUT);
-  digitalWrite(trig, LOW);
-  pinMode(echo, INPUT);
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+
+  pinMode(ECHO_PIN, INPUT);
 
   pinMode(LED_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
 
-  WiFi.begin(ssid, password);
+  setupMqttClient();
 
-  while (WiFi.status() != WL_CONNECTED)
+  while (!mqttClient.connected())
   {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-  }
-  Serial.println("Connected to the WiFi network");
+    Serial.println("Connecting to MQTT client...");
 
-  client.setServer(mqttServer, mqttPort);
-  client.setCallback(callback);
-
-  while (!client.connected())
-  {
-    Serial.println("Connecting to MQTT...");
-
-    if (client.connect("ESP8266Client", mqttUser, mqttPassword))
+    if (mqttClient.connect("captureMqtt", clientUser, clientPassword))
     {
 
       Serial.println("connected");
-      if (client.subscribe("capteur/led"))
+      if (mqttClient.subscribe("capteur/distance"))
       {
-        Serial.println("subscribed to the capteur/led");
+        Serial.println("subscribed to the capteur/distance");
       }
     }
     else
     {
 
       Serial.print("failed with state ");
-      Serial.println(client.state());
+      Serial.println(mqttClient.state());
       delay(2000);
     }
   }
 }
 
-void reconnect()
-{
-  while (!mqtt.connected())
-  {
-    Serial.println("[MQTT] Connecting to MQTT...");
-    if (mqtt.connect("MonObjectConnecte")) // Or mqtt.connect("MonObjectConnecte", "user", "pass") if you have protect the mqtt broker by credentials
-    {
-      Serial.println("[MQTT] Connected");
-      mqtt.publish("EssaieIOT", "Hello world from WizIO");
-      mqtt.subscribe("EssaieIOT");
-    }
-    else
-    {
-      Serial.print("[ERROR] MQTT Connect: ");
-      Serial.println(mqtt.state());
-      delay(60 * 1000);
-    }
+
+void publishDistance(){
+  //time_echo = pulseIn(ECHO_PIN, HIGH, 1000);
+  // distance en random
+  int rand_distance = random(0, 100);
+  distance = rand_distance;
+  Serial.print("time_echo");
+
+  mqttClient.publish("capteur/distance", String(distance).c_str());
+  Serial.println("pushed on the client with success");
+  if(distance <= 50){
+    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(BUZZER_PIN, HIGH);
+    mqttClient.publish("captureLED/ledStateTopic", String("on").c_str());
+    mqttClient.publish("captureLED/buzzerStateTopic", String("on").c_str());
+  }else {
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(BUZZER_PIN, LOW);
+    mqttClient.publish("captureLED/ledStateTopic", String("off").c_str());
+    mqttClient.publish("captureLED/buzzerStateTopic", String("off").c_str());
   }
+  if(distance > 50 || distance <= 0){
+    mqttClient.publish("captureOutRange/StateTopic", String("OutOfRangeDistance").c_str());
+    Serial.println("Out of distance");
+    Serial.print(distance);
+    Serial.println(" cm");
+  }else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }  
+}
+
+void subscribeDistance(){
+  mqttClient.subscribe("capteur/distance");
+  Serial.println("subscribe Distance in Capture topic");
 }
 
 void loop()
 {
-  digitalWrite(trig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trig, LOW);
-  lecture_echo = pulseIn(echo, HIGH);
-  cm = lecture_echo / 58;
-  Serial.print("Distance en cm :");
-  Serial.println(cm);
-  client.publish("capteur/distance", String(cm).c_str(), true); //Publie la température sur le topic temperature_topic
-  Serial.println("pushed on the client with success");
-  delay(1000);
-  client.loop();
+  digitalWrite(TRIG_PIN, HIGH);
+  delay(10);
+  digitalWrite(TRIG_PIN, LOW);
+  
+  publishDistance();  
+  delay(5000);
+  subscribeDistance();
+  mqttClient.loop();
 }
